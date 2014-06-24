9047916244a7791faf0584fe7b2353f04a701aa7
filipepoo2projeto <wfilipeb@gmail.com>
2014-06-23 21:44:07 -0300

    M/C (Classe)

---
+++ ProjetoLocadoraJava/src/br/com/fean/poo2/locadora/control/classe/classeService.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.fean.poo2.locadora.control.classe;
+
+import br.com.fean.poo2.locadora.modelo.funcionario.Funcionario;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Linali
+ */
+public interface classeService {
+    public void inserirFuncionario(Funcionario funcionario)  throws Exception;
+    public void alterarFuncionario(Funcionario funcionario) throws Exception;
+    public void deletarFuncionario(Funcionario funcionario) throws Exception;
+    public Funcionario retornarFuncionario(int id) throws Exception;
+    public Funcionario retornarFuncionario(String nome) throws Exception;
+    public ArrayList<Funcionario> retornarFuncionarios() throws Exception;  
+    
+}
---
+++ ProjetoLocadoraJava/src/br/com/fean/poo2/locadora/control/classe/classeServiceImpl.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.fean.poo2.locadora.control.classe;
+
+import br.com.fean.poo2.locadora.modelo.funcionario.Funcionario;
+import br.com.fean.poo2.locadora.modelo.funcionario.FuncionarioDAO;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Linali
+ */
+public class classeServiceImpl {
+     FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
+
+    /**
+     *
+     * @param funcionario
+     * @throws Exception
+     */
+    public void inserirFuncionario(Funcionario funcionario) throws Exception{
+        if (funcionario.getNome().length()>3){
+            funcionarioDAO.inserirFuncionario(funcionario);
+        }
+    }
+
+    public void alterarFuncionario(Funcionario funcionario) throws Exception {
+            funcionarioDAO.alterarFuncionario(funcionario);
+    }
+
+    public void deletarFuncionario(Funcionario funcionario) throws Exception {
+        funcionarioDAO.deletarFuncionario(funcionario);
+    }
+
+    public Funcionario retornarFuncionario(int id) throws Exception {
+        return funcionarioDAO.retornarFuncionario(id);
+    }
+     
+    public Funcionario retornarFuncionario(String nome) throws Exception {
+        return funcionarioDAO.retornarFuncionario(nome);
+    }
+
+   
+    public ArrayList<Funcionario> retornarFuncionarios() throws Exception  {
+        return funcionarioDAO.retornarFuncionarios();
+    }
+    
+}
+
+    
