418045efa435bad8ef00b474e00bf6ad22906025
Carolyne <lyne_2004_@hotmail.com>
2014-07-04 04:21:14 -0300

    titulo view (andamento)
    alguém sabe como utilizar o combobox com o hibernate? eu procurei na
    internet mas só acho exemplos onde se utiliza uma classe com os dados
    colocados nela...
    
    sem contar que eu já me estressei com a mensagem dizendo que string não
    pode ser convertida em midia, mas já mexi na classe titulo, midia,
    blabla e nada
    toString() inútil....
    
    se alguém puder dar uma olhadinha básica eu agradeceria :)

--- ProjetoLocadoraJava/src/br/com/fean/poo2/locadora/control/titulos/TituloServiceImpl.java
+++ ProjetoLocadoraJava/src/br/com/fean/poo2/locadora/control/titulos/TituloServiceImpl.java
@@ -1,5 +1,9 @@
 package br.com.fean.poo2.locadora.control.titulos;
 
+import br.com.fean.poo2.locadora.modelo.categoria.Categoria;
+import br.com.fean.poo2.locadora.modelo.classe.Classe;
+import br.com.fean.poo2.locadora.modelo.distribuidor.Distribuidor;
+import br.com.fean.poo2.locadora.modelo.midia.Midia;
 import br.com.fean.poo2.locadora.modelo.titulo.Titulo;
 import br.com.fean.poo2.locadora.modelo.titulo.TituloDAO;
 import java.util.ArrayList;
@@ -8,14 +12,32 @@ public class TituloServiceImpl implements TituloService {
     TituloDAO tituloDAO = new TituloDAO();
     
     @Override
-    public void inserirTitulo(Titulo titulo) throws Exception {
-        if (titulo.getNome().length() > 3) {
-            tituloDAO.inserirTitulo(titulo);
+    public void inserirTitulo(Integer codigo, String nome, Midia midia, Categoria genero, Midia nSerie, Classe classe, Midia estoque, Classe valor, Midia dataAquisicao, Distribuidor distribuidor) throws Exception {
+        if (nome.length() > 3) {
+             Titulo titulo = new Titulo();
+             titulo.setId(codigo);
+             titulo.setNome(nome);
+             titulo.setMidia(midia);
+             titulo.setCategorias(genero);
+             titulo.setMidia(nSerie);
+             titulo.setClasses(classe);
+             titulo.setMidia(estoque);
+             titulo.setClasses(valor);
+             titulo.setMidia(dataAquisicao);
+             titulo.setDistribuidor(distribuidor);
+             tituloDAO.inserirTitulo(titulo);
         }
     }
 
     @Override
-    public void alterarTitulo(Titulo titulo) throws Exception {
+    public void alterarTitulo(Integer codigo, String nome, Midia nSerie, Classe classe, Classe valor, Distribuidor distribuidor) throws Exception {
+        Titulo titulo = new Titulo();
+        titulo.setId(codigo);
+        titulo.setNome(nome);
+        titulo.setMidia(nSerie);
+        titulo.setClasses(classe);
+        titulo.setClasses(valor);
+        titulo.setDistribuidor(distribuidor);
         tituloDAO.alteraTitulo(titulo);
     }
 
